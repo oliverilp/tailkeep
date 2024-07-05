f257aaf782ae0bd3145449ac23a27f4873068a90
oliverilp <oliver@oliverilp.ee>
2024-07-05 13:42:08 +0300

    Add video filename and channel data to DB

--- apps/worker/src/metadata-fetcher.ts
+++ apps/worker/src/metadata-fetcher.ts
@@ -5,12 +5,15 @@ export interface Metadata {
   url: string;
   title: string;
   uploader: string;
+  channelId: string;
+  channelUrl: string;
   durationString: string;
   duration: number;
   thumbnailUrl: string;
   description: string;
   viewCount: number;
   commentCount: number;
+  filename: string;
 }
 
 export type ProgressCallback = (data: Metadata) => void;
@@ -38,12 +41,15 @@ export class MetadataFetcher {
       url: json.webpage_url,
       title: json.title,
       uploader: json.uploader,
+      channelId: json.channel_id,
+      channelUrl: json.channel_url,
       durationString: json.duration_string,
       duration: json.duration,
       thumbnailUrl: json.thumbnail,
       description: json.description,
       viewCount: json.view_count,
-      commentCount: json.comment_count
+      commentCount: json.comment_count,
+      filename: json.filename
     };
 
     return output;
