7122db861bdff8f0519686c5860ad68a98330663
Oliver Ilp <oliver@oliverilp.ee>
2024-09-10 14:50:48 +0300

    Fix UI errors

--- apps/server/src/app/dashboard/logs/page.tsx
+++ apps/server/src/app/dashboard/logs/page.tsx
@@ -1,22 +1,15 @@
-import { Button } from '@/components/ui/button';
-import { Download, Link, FileClock } from 'lucide-react';
+import { FileClock } from 'lucide-react';
 import React from 'react';
 
 function Logs() {
   return (
     <div className="grid h-full w-full place-items-center">
       <div className="flex w-full flex-col items-center">
-        <FileClock className="text-muted-foreground mb-2 h-12 w-12" />
+        <FileClock className="mb-2 h-12 w-12 text-muted-foreground" />
         <div className="font-semibold">Logs page</div>
         <div className="text-muted-foreground">
           This feature is coming soon™.
         </div>
-        <Link className="pt-6" href="/dashboard/download">
-          <Button>
-            <Download className="mr-2 h-4 w-4" />
-            <span>Download Video</span>
-          </Button>
-        </Link>
       </div>
     </div>
   );
--- apps/server/src/app/dashboard/subscriptions/page.tsx
+++ apps/server/src/app/dashboard/subscriptions/page.tsx
@@ -1,22 +1,15 @@
-import { Button } from '@/components/ui/button';
-import { Download, Link, ListVideo } from 'lucide-react';
+import { ListVideo } from 'lucide-react';
 import React from 'react';
 
 function Subscriptions() {
   return (
     <div className="grid h-full w-full place-items-center">
       <div className="flex w-full flex-col items-center">
-        <ListVideo className="text-muted-foreground mb-2 h-12 w-12" />
+        <ListVideo className="mb-2 h-12 w-12 text-muted-foreground" />
         <div className="font-semibold">Subscriptions page</div>
         <div className="text-muted-foreground">
           This feature is coming soon™.
         </div>
-        <Link className="pt-6" href="/dashboard/download">
-          <Button>
-            <Download className="mr-2 h-4 w-4" />
-            <span>Download Video</span>
-          </Button>
-        </Link>
       </div>
     </div>
   );
